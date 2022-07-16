3e9499ac3016b3f20ce83c5faa7f04f9808f2ceb
cheemis <nnolasco@calpoly.edu>
2022-07-15 21:42:14 -0700

    fnaf walls added

---
+++ Assets/Scripts/NPCs/NPCPatrol.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCPatrol : MonoBehaviour
+{
+
+    //Components
+    private Animator anim;
+    private NavMeshAgent nav;
+
+
+    //Patrolling Variables
+    public Vector2 idleTimes = new Vector2(1, 10);
+    private Vector3 dest;
+    public Vector2 range = new Vector2(50, 100);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(transform.position, out hit, 50, NavMesh.AllAreas);
+        nav.Warp(hit.position);
+
+        if (Random.Range(0, 100) > 50)
+        {
+            Debug.Log("idle");
+            StartCoroutine(Idle(Random.Range(idleTimes.x, idleTimes.y)));
+        }
+        else
+        {
+            Debug.Log("travel");
+            SetNewDestination();
+        }
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("speed", nav.velocity.magnitude);
+    }
+
+    //this method sets a new destination for the NPC to wander to
+    private void SetNewDestination()
+    {
+        NavMeshHit hit;
+
+        Vector2 rand = Random.insideUnitCircle * Random.Range(range.x, range.y);
+        Vector3 pos = new Vector3(rand.x, 0, rand.y);
+
+        NavMesh.SamplePosition(pos, out hit, 50, NavMesh.AllAreas);
+
+        dest = hit.position;
+        Debug.Log(dest);
+        Debug.Log(nav.SetDestination(dest));
+
+        StartCoroutine(Travelling());
+    }
+
+    //this coroutine waits at a destination before moving to a new position
+    IEnumerator Idle(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SetNewDestination();
+    }
+
+    //this coroutine checks if the NPC has reached it's destination
+    IEnumerator Travelling()
+    {
+        while(Vector3.Distance(transform.position, dest) > 1)
+        {
+            yield return 0;
+        }
+        yield return 0;
+        StartCoroutine(Idle(Random.Range(idleTimes.x, idleTimes.y)));
+    }
+}
