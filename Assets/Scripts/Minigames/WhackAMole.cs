97714eb28b31be418c5a13dd46290d8dbca295ba
Jacob Morales <jacobmorales2001@outlook.com>
2022-07-16 14:13:39 -0700

    Added WhackAMole Game

--- Assets/Scripts/Minigames/WhackAMole.cs
+++ Assets/Scripts/Minigames/WhackAMole.cs
@@ -6,15 +6,44 @@ using UnityEngine.Events;
 public class WhackAMole : MonoBehaviour
 {
     [SerializeField] private float SecondsBetweenChecks = 1.0f;
-    private float _timer = 0.0f;
     [SerializeField] private int MoleChance = 4; 
+    [SerializeField] private int MolesToWin = -1;
     [SerializeField] List<GameObject> Whackamoles = default;
+    [SerializeField] UnityEvent AnyGoalReachedEvent = default;
+    [SerializeField] UnityEvent AllGoalsReachedEvent = default;
+    
+    private float _timer = 0.0f;
+    private bool _timerEnabled = true;
+
+    public void UpdateMoleCount()
+    {
+        if (MolesToWin == -1)
+        {
+            MolesToWin = Whackamoles.Count;
+        }
+
+        MolesToWin--;
+        if (MolesToWin < 1)
+        {
+            AllGoalsReachedEvent.Invoke();
+        }
+    }
+
+    private void Start() 
+    {
+        foreach (GameObject mole in Whackamoles)
+        {
+            GetComponent<WhackAMoleMole>().GoalReachedEvent.AddListener(AnyGoalReachedEvent.Invoke);
+        }
+
+        AnyGoalReachedEvent.AddListener(UpdateMoleCount);
+    }
 
     private void Update()
     {
         _timer += Time.deltaTime;
 
-        if (_timer >= SecondsBetweenChecks)
+        if (_timerEnabled && _timer >= SecondsBetweenChecks)
         {
             _timer = 0.0f;
             if (Random.Range(1, MoleChance) == 1)
