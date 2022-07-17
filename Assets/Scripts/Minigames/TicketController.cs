86b2e05217a3e1edfd2d6b6cd52b1fae84342e9a
IlanGeerligs <interflashz@gmail.com>
2022-07-17 20:20:25 +0200

    Added minimum timer tickets

--- Assets/Scripts/Minigames/TicketController.cs
+++ Assets/Scripts/Minigames/TicketController.cs
@@ -9,11 +9,19 @@ public class TicketController : MonoBehaviour
     [SerializeField] private AudioClip ticketSpawnSound;
     private GameObject _spawnedTicket = default;
 
+    private float minTimeBetweenSpawns = 0.5f;
+    private float lastSpawnTime = 0f;
+
     public void SpawnTicket()
     {
-        _spawnedTicket = Instantiate(TicketPrefab, transform);
-        _spawnedTicket.transform.Translate(Random.Range(0.0f, randomScale), 0, Random.Range(0.0f, randomScale));
+        if (Time.time > lastSpawnTime + minTimeBetweenSpawns)
+        {
+            _spawnedTicket = Instantiate(TicketPrefab, transform);
+            _spawnedTicket.transform.Translate(Random.Range(0.0f, randomScale), 0, Random.Range(0.0f, randomScale));
 
-        AudioSource.PlayClipAtPoint(ticketSpawnSound, _spawnedTicket.transform.position, 60f);
+            AudioSource.PlayClipAtPoint(ticketSpawnSound, _spawnedTicket.transform.position, 60f);
+
+            lastSpawnTime = Time.time;
+        }
     }
 }
